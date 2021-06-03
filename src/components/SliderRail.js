241166b949806c4539596c587780dc67201678ab
Paul <paulbanwell@beyondaerospace.com>
2021-06-03 16:51:50 -0700

    fix ref

--- src/components/SliderRail.js
+++ src/components/SliderRail.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const SliderRail = ({ getRailProps }) => (
+export const SliderRail = React.forwardRef(({ getRailProps }, ref) => (
   <>
-    <div className='react_time_range__rail__outer' {...getRailProps()} />
+    <div ref={ref} className='react_time_range__rail__outer' {...getRailProps()} />
     <div className='react_time_range__rail__inner' />
   </>
-)
+));
 
 SliderRail.propTypes = { getRailProps: PropTypes.func.isRequired }
 
